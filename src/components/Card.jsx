b256e0f1645a1c8709e18003c2d5f7b51ec88de8
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2024-05-17 12:02:23 +0545

    added dialog box when user add items to cart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,10 @@ import {
   Typography,
   Button,
   Rating,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
 } from "@material-tailwind/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -15,103 +19,138 @@ export function EcommerceCard() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [cartCount, setCartCount] = useState(0);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [currentItem, setCurrentItem] = useState(null);
 
   useEffect(() => {
-    // // Load initial count from localStorage
     const storedCount = localStorage.getItem("cartCount");
     if (storedCount) {
       setCartCount(parseInt(storedCount, 10));
     }
 
-    // Define the async function to fetch data
     const fetchData = async () => {
       try {
-        // Make the API call using Axios
         const response = await axios.get("https://fakestoreapi.com/products");
-        // Update the state with the fetched data
         setData(response.data);
       } catch (err) {
-        // Handle any errors
         setError(err);
       } finally {
-        // Set loading to false after the data is fetched
         setLoading(false);
       }
     };
 
-    // Call the fetchData function
     fetchData();
   }, []);
 
   const handleAddToCart = (item) => {
-    const newCount = cartCount + 1;
-    setCartCount(newCount);
-    localStorage.setItem("cartCount", newCount);
+    console.log("Adding item to cart:", item);
+
     // Get existing cart items from localStorage
     const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
-
     cartItems.push(item);
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
+
+    // Update the cart count
+    const newCount = cartItems.length;
+    setCartCount(newCount);
+    localStorage.setItem("cartCount", newCount);
+    console.log("New cart count:", newCount);
 
     // Dispatch a custom event to notify the cart count change
     const event = new CustomEvent("cartCountUpdated", { detail: newCount });
     window.dispatchEvent(event);
+
+    setCurrentItem(item);
+    setDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setDialogOpen(false);
+    setCurrentItem(null);
   };
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <section className="flex items-center justify-center flex-wrap gap-5 ">
-      {data?.map((item) => (
-        <Card className="w-56 h-96" key={item.id}>
-          <CardHeader shadow={false} floated={false} className="h-96">
-            <img
-              src={item.image}
-              alt="card-image"
-              className=" w-full h-full object-contain"
-            />
-          </CardHeader>
-          <CardBody>
-            <div className="mb-2 flex items-center justify-between">
-              <Typography color="blue-gray" className="font-medium text-sm">
-                {item.title.slice(0, 20)}..
+    <>
+      <section className="flex items-center justify-center flex-wrap gap-5">
+        {data?.map((item) => (
+          <Card className="w-56 h-96" key={item.id}>
+            <CardHeader shadow={false} floated={false} className="h-96">
+              <img
+                src={item.image}
+                alt="card-image"
+                className=" w-full h-full object-contain"
+              />
+            </CardHeader>
+            <CardBody>
+              <div className="mb-2 flex items-center justify-between">
+                <Typography color="blue-gray" className="font-medium text-sm">
+                  {item.title.slice(0, 20)}..
+                </Typography>
+                <Typography color="blue-gray" className="font-medium">
+                  {item.price}$
+                </Typography>
+              </div>
+              <Typography
+                variant="small"
+                color="gray"
+                className="font-normal opacity-75 text-xs"
+              >
+                {item?.description.slice(0, 50)}...
               </Typography>
-              <Typography color="blue-gray" className="font-medium">
-                {item.price}$
+              <div className="flex my-3">
+                <span className="font-semibold">Rating: </span>
+                {typeof item?.rating?.rate === "number" &&
+                  item.rating.rate >= 0 &&
+                  item.rating.rate <= 5 && (
+                    <Rating
+                      className="text-md"
+                      value={Math.floor(item.rating.rate)}
+                    />
+                  )}
+              </div>
+            </CardBody>
+            <CardFooter className="pt-0">
+              <Button
+                ripple={false}
+                fullWidth={true}
+                className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                onClick={() => handleAddToCart(item)}
+              >
+                Add to Cart
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </section>
+
+      <Dialog open={dialogOpen} handler={closeDialog}>
+        <DialogHeader>Item Added to Cart</DialogHeader>
+        <DialogBody>
+          {currentItem && (
+            <>
+              <img
+                src={currentItem.image}
+                alt={currentItem.title}
+                className="h-24 mx-auto"
+              />
+              <Typography variant="h6" className="mt-4">
+                {currentItem.title}
               </Typography>
-            </div>
-            <Typography
-              variant="small"
-              color="gray"
-              className="font-normal opacity-75 text-xs"
-            >
-              {item?.description.slice(0, 50)}...
-            </Typography>
-            <div className="flex my-3">
-              <span className="font-semibold">Rating: </span>
-              {typeof item?.rating?.rate === "number" &&
-                item.rating.rate >= 0 &&
-                item.rating.rate <= 5 && (
-                  <Rating
-                    className="text-md"
-                    value={Math.floor(item.rating.rate)}
-                  /> // Convert to integer
-                )}
-            </div>
-          </CardBody>
-          <CardFooter className="pt-0">
-            <Button
-              ripple={false}
-              fullWidth={true}
-              className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
-              onClick={() => handleAddToCart(item)}
-            >
-              Add to Cart
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
-    </section>
+              <Typography className="mt-2">
+                Price: {currentItem.price}$
+              </Typography>
+            </>
+          )}
+        </DialogBody>
+        <DialogFooter>
+          <Button variant="gradient" color="blue" onClick={closeDialog}>
+            Continue Shopping
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
   );
 }
