08952398312131aabee3387385ea07e3c5b7b728
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2024-06-05 22:02:37 +0545

    base Url changed

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5055/api/p1";
+// const BASE_URL = "http://localhost:5055/api/p1";
+const BASE_URL = "https://hamropasal-backend-8otn.onrender.com/api/p1";
 
 const axiosInstance = axios.create();
 
